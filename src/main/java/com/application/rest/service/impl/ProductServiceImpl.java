09edf1910ada2b001c897dd7bdaf0a74857bf992
Anuar Orozco <anuarorozzco@gmail.com>
2024-11-19 18:57:01 -0600

    Created service and impl packages with the interfaces of service and its implementations

--- src/main/java/com/application/rest/service/impl/ProductServiceImpl.java
+++ src/main/java/com/application/rest/service/impl/ProductServiceImpl.java
@@ -1,43 +1,43 @@
-package com.application.rest.persistence.impl;
+package com.application.rest.service.impl;
 
 import com.application.rest.entities.Product;
 import com.application.rest.persistence.IProductDAO;
-import com.application.rest.repository.IProductRepository;
+import com.application.rest.service.IProductService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
 import java.util.List;
 import java.util.Optional;
 
-@Component
-public class ProductDAOImpl implements IProductDAO {
+@Service
+public class ProductServiceImpl implements IProductService {
 
     @Autowired
-    IProductRepository productRepository;
+    private IProductDAO productDAO;
 
     @Override
     public List<Product> findAll() {
-        return (List<Product>) productRepository.findAll();
+        return productDAO.findAll();
     }
 
     @Override
     public Optional<Product> findById(Long id) {
-        return productRepository.findById(id);
+        return productDAO.findById(id);
     }
 
     @Override
     public void save(Product product) {
-        productRepository.save(product);
+        productDAO.save(product);
     }
 
     @Override
     public void deleteById(Long id) {
-        productRepository.deleteById(id);
+        productDAO.deleteById(id);
     }
 
     @Override
     public List<Product> findByPriceInRange(BigDecimal minPrice, BigDecimal maxPrice) {
-        return productRepository.findProductByPriceInRange(minPrice, maxPrice);
+        return productDAO.findByPriceInRange(minPrice, maxPrice);
     }
 }
